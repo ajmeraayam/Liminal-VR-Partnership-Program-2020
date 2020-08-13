cfa63e892a7d72c6c221239527a30ed4f0b50097
ajmeraayam <ajmeraayam@gmail.com>
2020-08-13 15:58:26 +1000

    Recording mouse click to start the circular ladle movement

--- Alchemy/Assets/Scripts/LadleAnimation.cs
+++ Alchemy/Assets/Scripts/LadleAnimation.cs
@@ -4,20 +4,50 @@ using UnityEngine;
 
 public class LadleAnimation : MonoBehaviour
 {
-    float timeCounter = 0f;
-    float radius = 0.30f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.position = new Vector3(transform.position.x, 0.3f, transform.position.z);
-    }
+    private float timeCounter = 0f;
+    private float radius = 0.30f;
+    private bool clickBlocked = false;
 
     // Update is called once per frame
     void Update()
     {
-        timeCounter += Time.deltaTime * 2.5f;
-        float z = Mathf.Cos (timeCounter) * radius;
-        float x = Mathf.Sin (timeCounter) * radius - 0.05f;
+        // When left mouse button is clicked and clicking is unblocked
+        if(Input.GetMouseButtonDown(0) && !clickBlocked)
+        {
+            //Move the ladle down in Y-axis
+            transform.position = new Vector3(transform.position.x, 0.3f, transform.position.z);
+            //Start the coroutine to move the ladle in circular motion
+            StartCoroutine(invokeAnimation());
+        }
+    }
+
+    //Invokes ladle animation for 6 seconds
+    IEnumerator invokeAnimation()
+    {
+        //During the time of animation, mouse clicking is blocked
+        clickBlocked = true;
+        float timePassed = 0f;
+        //For 6 seconds, keep calling moveLadle method
+        while(timePassed < 6f)
+        {
+            Invoke("moveLadle", 0.0001f);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+        //After 6 seconds, unblock the clicking so user can animate ladle again 
+        clickBlocked = false;
+        //Reset the position of the ladle
+        transform.position = new Vector3(0.3f, 0.6f, 0f);
+    }
+
+    //Generate the circular motion using cos and sin values.
+    private void moveLadle()
+    {
+        //Speed of ladle is set to PI, so the ladle will complete a circle in 2 seconds
+        timeCounter -= Time.deltaTime * Mathf.PI;
+        //Move the ladle in x and z axis, in circular motion
+        float x = Mathf.Cos (timeCounter) * radius;
+        float z = Mathf.Sin (timeCounter) * radius;
         float y = transform.position.y;
         transform.position = new Vector3 (x, y, z);
     }
