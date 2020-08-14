48634182277cb0c235e3e6a585de0173477340c5
Harlem-Lan-Ting <63002557+Harlem-Lan-Ting@users.noreply.github.com>
2020-08-14 14:25:23 +1000

    Implament the fire

---
+++ Alchemy/Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public ParticleSystem particle;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            particle.Play();
+        }
+    }
+}
