2656538e7aded8d54150e0b67daa4891b858c68c
ajmeraayam <ajmeraayam@gmail.com>
2020-08-14 21:53:22 +1000

    Scripts updated

--- Alchemy/Assets/Scripts/ControllerScript.cs
+++ Alchemy/Assets/Scripts/ControllerScript.cs
@@ -3,15 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Liminal.SDK.VR;
 using Liminal.SDK.VR.Input;
-<<<<<<< Updated upstream
-//using UnityEngine.EventSystems;
-//using Liminal.SDK.VR.EventSystems;
-
-
-//IPointerClickHandler to detect pointer click
-public class ControllerScript : MonoBehaviour
-{
-=======
 
 /*Performance improvements - Cache methods for gameobjects (probably in hashmap) that are used often in the game. It will save the time to search for script in the gameobject. 
 Another way is to check if OnPointerClick, OnPointerDown and OnPointerUp does the job better or not
@@ -20,7 +11,6 @@ public class ControllerScript : MonoBehaviour
 {
     //To disable input if needed
     bool disableInput = false;
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -28,45 +18,6 @@ public class ControllerScript : MonoBehaviour
     }
 
     // Update is called once per frame
-<<<<<<< Updated upstream
-    /*void FixedUpdate()
-    {
-        RaycastHit hit;
-        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10f;
-        Ray myray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        Debug.DrawRay(transform.position, forward, Color.red);
-
-        if(Physics.Raycast(myray, out hit, 100f))
-        {    
-            if(hit.collider != null )
-            {
-                //hit.collider.gameObject.GetComponent<LadleAnimation>().invokeAnimation();
-                print(hit.collider.tag);
-            }
-        }
-    }*/
-    void Update()
-    {
-        var vrDevice = VRDevice.Device;
-
-        if(vrDevice == null)
-            return;
-
-        var hitResult = vrDevice.PrimaryInputDevice.Pointer.CurrentRaycastResult;
-        if(hitResult.gameObject.CompareTag("Ladle"))
-        {
-            print("Ladle");
-        }
-
-    }
-
-    /*public void OnPointerClick(PointerEventData eventData)
-    {
-        var raycast_result = eventData.pointerCurrentRaycast;
-        print(raycast_result.gameObject.tag);
-    }*/
-=======
     void Update()
     {
         //Reference to VR device that is being used
@@ -116,5 +67,4 @@ public class ControllerScript : MonoBehaviour
         disableInput = value;
     }
 
->>>>>>> Stashed changes
 }
--- Alchemy/Assets/Scripts/LadleAnimation.cs
+++ Alchemy/Assets/Scripts/LadleAnimation.cs
@@ -8,30 +8,6 @@ public class LadleAnimation : MonoBehaviour
     private float radius = 0.30f;
     private bool clickBlocked = false;
     private Vector3 startPosition;
-<<<<<<< Updated upstream
-
-    // Update is called once per frame
-    void Start()
-    {
-        startPosition = transform.localPosition;
-    }
-
-    /*void OnMouseDown()
-    {
-        // When left mouse button is clicked and clicking is unblocked
-        if(Input.GetMouseButtonDown(0) && !clickBlocked)
-        {
-            float yPos = transform.localPosition.y - 0.3f;
-            //Move the ladle down in Y-axis
-            transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
-            //Start the coroutine to move the ladle in circular motion
-            StartCoroutine(invokeAnimation());
-        }
-    }*/
-
-    public void WhenClicked()
-    {
-=======
     private ControllerScript controllerScript;
 
     // Start is called before the first frame update
@@ -46,7 +22,6 @@ public class LadleAnimation : MonoBehaviour
     {
         //Send message to disable inputs from the controller when ladle is moving
         controllerScript.SendMessage("DisableInput", true);
->>>>>>> Stashed changes
         float yPos = transform.localPosition.y - 0.3f;
         //Move the ladle down in Y-axis
         transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
@@ -57,11 +32,6 @@ public class LadleAnimation : MonoBehaviour
     //Invokes ladle animation for 6 seconds
     IEnumerator invokeAnimation()
     {
-<<<<<<< Updated upstream
-        //During the time of animation, mouse clicking is blocked
-        clickBlocked = true;
-=======
->>>>>>> Stashed changes
         float timePassed = 0f;
         //For 6 seconds, keep calling moveLadle method
         while(timePassed < 6f)
@@ -70,17 +40,10 @@ public class LadleAnimation : MonoBehaviour
             timePassed += Time.deltaTime;
             yield return null;
         }
-<<<<<<< Updated upstream
-        //After 6 seconds, unblock the clicking so user can animate ladle again 
-        clickBlocked = false;
-        //Reset the position of the ladle
-        transform.localPosition = startPosition;
-=======
         //Reset the position of the ladle
         transform.localPosition = startPosition;
         //After 6 seconds, unblock the input so user can continue playing  
         controllerScript.SendMessage("DisableInput", false);
->>>>>>> Stashed changes
     }
 
     //Generate the circular motion using cos and sin values.
