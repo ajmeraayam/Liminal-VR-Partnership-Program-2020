20a8337cc05394541c6a4d270958024d51637529
ajmeraayam <ajmeraayam@gmail.com>
2020-09-16 18:58:49 +1000

    Level handling according to the streak of correct and wrong recipes implemented

---
+++ Alchemy/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHandler : MonoBehaviour
+{
+    // Stores the current level (1 to 5)
+    private int currentRecipeLevel;
+    private int correctStreak;
+    private int consecWrongStreak;
+    // TRUE if last recipe was correct, FALSE otherwise
+    private bool lastRecipe;
+
+
+    // Manages level of recipes to be generated
+    public void changeRecipeLevel()
+    {
+        if(currentRecipeLevel == 1)
+        {
+            if(correctStreak == 3)
+            {
+                currentRecipeLevel++;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+        }
+        else if(currentRecipeLevel == 2)
+        {
+            if(correctStreak == 5)
+            {
+                currentRecipeLevel++;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+            if(consecWrongStreak == 5)
+            {
+                currentRecipeLevel--;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+        }
+        else if(currentRecipeLevel == 3)
+        {
+            if(correctStreak == 15)
+            {
+                currentRecipeLevel++;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+            if(consecWrongStreak == 5)
+            {
+                currentRecipeLevel--;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+        }
+        else if(currentRecipeLevel == 4)
+        {
+            if(correctStreak == 20)
+            {
+                currentRecipeLevel++;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+            if(consecWrongStreak == 4)
+            {
+                currentRecipeLevel--;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+        }
+        else if(currentRecipeLevel == 5)
+        {
+            if(consecWrongStreak == 3)
+            {
+                currentRecipeLevel--;
+                correctStreak = 0;
+                consecWrongStreak = 0;
+            }
+        }
+    }
+
+    // Call this method when player makes a wrong recipe. This method will take care of streaks of potions
+    public void wrongRecipe()
+    {
+        if(!lastRecipe)
+        {
+            consecWrongStreak++;
+            lastRecipe = false;
+        }
+        else
+        {
+            consecWrongStreak = 1;
+            correctStreak = 0;
+            lastRecipe = false;
+        }
+    }
+
+    // Call this method when player makes a correct recipe. This method will take care of streaks of potions
+    public void correctRecipe()
+    {
+        if(lastRecipe)
+        {
+            correctRecipe++;
+            lastRecipe = false;
+        }
+        else
+        {
+            consecWrongStreak = 0;
+            correctRecipe = 1;
+            lastRecipe = false;
+        }
+    }
+}
