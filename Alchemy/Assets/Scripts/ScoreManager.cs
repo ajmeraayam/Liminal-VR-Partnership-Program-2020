bee810dada5434baef3857077eecedb3ad0babdf
ajmeraayam <ajmeraayam@gmail.com>
2020-09-29 20:29:34 +1000

    Recipe completion timer added and recipe display board included

---
+++ Alchemy/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    // UI elements
+    public Text streak;
+    public Text totalScore;
+    private LevelHandler levelHandlerScript;
+
+    void Start()
+    {
+        levelHandlerScript = GetComponent<LevelHandler>();
+        totalScore.text = "Total: " + 0;
+        streak.text = "Total: " + 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int total = levelHandlerScript.Score;
+        int current = levelHandlerScript.CorrectStreak;
+        totalScore.text = "Total: " + total;
+        streak.text = "Current: " + current;
+    }
+}
